67c23cd81ff5289053ac072446561126921319db
duythain1411-art <duythain104@gmail.com>
2026-02-05 17:34:53 +0700

    Update Core.py

--- Study-engine/Engine/Core.py
+++ Study-engine/Engine/Core.py
@@ -4,3 +4,24 @@ class Question:
         self.q_type = q_type
         self.data = data
         self.text = text
+from modules.math import MathModule
+
+
+class Engine:
+    def run(self, question: Question):
+        focus = self.find_focus(question.text)
+        module = self.select_module(question.subject, focus)
+        result = module.solve(question.data)
+        return result
+
+    def find_focus(self, text):
+        keywords = ["chu vi", "diện tích", "tính", "tìm"]
+        for k in keywords:
+            if k in text.lower():
+                return k
+        return None
+
+    def select_module(self, subject, focus):
+        if subject == "Toán":
+            return MathModule(focus)
+        raise Exception("Không có module phù hợp")
