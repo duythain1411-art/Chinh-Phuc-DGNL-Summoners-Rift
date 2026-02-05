49f9427cffa6ed9b5760676ce4c4e762194c91d4
duythain1411-art <duythain104@gmail.com>
2026-02-05 17:33:11 +0700

    Create Core.py

---
+++ Study-engine/Engine/Core.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self, subject, q_type, data, text):
+        self.subject = subject
+        self.q_type = q_type
+        self.data = data
+        self.text = text
